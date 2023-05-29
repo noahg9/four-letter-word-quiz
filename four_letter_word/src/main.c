802936ac7e91fc8977e09652ee9fc4d82ff24e99
Noah <noah.guerin@student.kdg.be>
2023-05-29 14:24:40 +0200

    Get random word to display

--- four_letter_word/src/main.c
+++ four_letter_word/src/main.c
@@ -1,21 +1,39 @@
 #include <avr/io.h>
-#include <usart.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <led.h>
-#include <display.h>
-#include <button.h>
-#include <potentio.h>
-#include <buzzer.h>
 #include <time.h>
+#include <ctype.h>
+#include "usart.h"
+#include "led.h"
+#include "display.h"
+#include "button.h"
+#include "potentio.h"
+#include "buzzer.h"
+
+#define __DELAY_BACKWARD_COMPATIBLE__
+
 
 char* catString[7] = {"ANML", "HMAN", "CTRY", "THNG"};
-char* animalWords[7] = {"DUCK", "BEAR", "LION", "FROG", "FISH", "BIRD", "DEER"};
-char* humanWords[7] = {"HAND", "HEAD", "FOOT", "NOSE", "BACK", "KNEE", "CHIN"};
-char* countryWords[7] = {"IRAN", "PERU", "CUBA", "CHAD", "FIJI", "LAOS", "TOGO"};
-char* thingWords[7] = {"BOOK", "BALL", "DESK", "DOOR", "FORK", "LAMP", "SHOE"};
+char* wordCategories[4][7] = {
+  {"DUCK", "BEAR", "LION", "FROG", "FISH", "BIRD", "DEER"},
+  {"HAND", "HEAD", "FOOT", "NOSE", "BACK", "KNEE", "CHIN"},
+  {"IRAN", "PERU", "CUBA", "CHAD", "FIJI", "LAOS", "TOGO"},
+  {"BOOK", "BALL", "DESK", "DOOR", "FORK", "LAMP", "SHOE"}
+};
+
+void hideConsonants(const char *secretWord, char *visibleWord) {
+    int length = strlen(secretWord);
+    for (int i = 0; i < length; i++) {
+        visibleWord[i] = toupper(secretWord[i]);  // Convert the character to uppercase for case-insensitive comparison
+        if (!(visibleWord[i] == 'A' || visibleWord[i] == 'E' || visibleWord[i] == 'I' || visibleWord[i] == 'O' || visibleWord[i] == 'U')) {
+            visibleWord[i] = '_';
+        }
+    }
+    visibleWord[length] = '\0';  // Add null terminator to the modified word
+}
 
 void correctSound() {
   enableBuzzer();
@@ -27,33 +45,44 @@ void incorrectSound() {
   playTone(C5, 100);
 }
 
+
 int main() {
   initUSART();
   enableButton(1);
   enableButton(2);
   enableButton(3);
   initDisplay();
-  srand(time(NULL));
+  srand(0);
+
   printf("Welcome to the four-letter word quiz!");
+  
   while (!buttonPushed(1)) {
     writeString("CAT?");
   }
+
   printf("\n1 - Next category\n2 - Select category \nCategories: animal (ANML), human (HMAN), country (CTRY), thing (THNG)");
-  int catID = 0;
+  int catID = -1;
+
   while (!buttonPushed(2)) {
+    catID++;
     _delay_ms(500);
     while (!buttonPushed(1) && !buttonPushed(2)) {
       writeString(catString[catID]);
     }
-    catID++;
     if (catID == 4 && !buttonPushed(2)) {
       catID = 0;
     }
   }
-  int randIndex = rand() % 7;
-  char* secretWord = animalWords[randIndex];
-  while (1) {
-    writeString(secretWord);
+  
+  char* secretWord = wordCategories[catID][rand() % 7];
+  
+  char visibleWord[5];
+
+  hideConsonants(secretWord, visibleWord);
+
+  while (!buttonPushed(1) && !buttonPushed(2) && !buttonPushed(3)) {
+    writeString(visibleWord);
   }
+
   return 0;
 }
