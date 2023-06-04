9fb8d871ac72d1bbb80240adead7fb401074f902
Noah <noah.guerin@student.kdg.be>
2023-06-04 15:53:49 +0200

    Remove timer library

--- four_letter_word/src/main.c
+++ four_letter_word/src/main.c
@@ -11,7 +11,6 @@
 #include <button.h>
 #include <potentio.h>
 #include <buzzer.h>
-#include <timer.h>
 
 // Constants
 #define MAX_CATEGORIES 6
@@ -26,21 +25,19 @@ typedef struct {
   int time;
 } PUZZLE;
 
-int stage = 1;
-int cat_id = 0;
+int cat_id;
 char visible_word[MAX_WORD_LENGTH];
-int button1_index = 0;
-int button2_index = 0;
-int button3_index = 0;
+int button1_index;
+int button2_index;
+int button3_index;
 uint16_t adc_value = 0;
-
-volatile int seed_time = 0;
-volatile int verify_time = 0;
-volatile int seed_set = 0;
-volatile int button_pressed = 0;
-volatile int cat_selected = 0;
-volatile int game_active = 0;
-volatile int game_won = 0;
+int seed_time;
+int verify_time;
+int seed_set;
+int button_pressed;
+int cat_selected;
+int game_active;
+int game_won;
 
 PUZZLE puzzle;
 
@@ -143,6 +140,34 @@ ISR(ADC_vect)
   adc_value = ADC;
 }
 
+void initTimer0() {
+  // Set timer 0 to CTC mode
+  TCCR0A |= _BV(WGM01);
+
+  // Set prescaler to 64
+  TCCR0B |= _BV(CS01) | _BV(CS00);
+
+  // Set compare match register to generate interrupt every 1 millisecond
+  OCR0A = 249; // Assuming a 16MHz clock and prescaler of 64 (16,000,000 / 64 / 1000 - 1)
+
+  // Enable compare match A interrupt
+  TIMSK0 |= _BV(OCIE0A);
+}
+
+void initTimer1()
+{
+    // Set timer 1 to CTC mode
+  TCCR1B |= _BV(WGM12);
+
+  // Set prescaler to 1024
+  TCCR1B |= _BV(CS12) | _BV(CS10);
+
+  OCR1A = 15624; // Set OCR0A for 1-second interval
+
+  // Enable compare match A interrupt
+  TIMSK1 |= _BV(OCIE1A);
+}
+
 void hideConsonants(const char *word, char *visible_word) 
 {
   for (int i = 0; i < MAX_WORD_LENGTH-1; i++) 
@@ -205,19 +230,7 @@ void incorrectSound()
   playTone(C5, 1000);
 }
 
-void setup()
-{
-  button_pressed = 0;
-  cat_selected = 0;
-  game_active = 0;
-  game_won = 0;
-  cat_id = 0;
-  verify_time = 0;
-}
-
-int main() 
-{
-  
+void setup() {
   initUSART();
   initDisplay();
   initADC();
@@ -231,82 +244,112 @@ int main()
 
   initTimer0();
   initTimer1();
+}
+
+void reset()
+{
+  button_pressed = 0;
+  cat_selected = 0;
+  game_active = 0;
+  game_won = 0;
+  cat_id = 0;
+  verify_time = 0;
+}
+
+void start()
+{
+  reset();
+
+  printf("\n\nWelcome to the four-letter word quiz.");
+  printf("\nButton 1 - Begin");
+
+  while (!button_pressed) 
+  {
+    writeString("CAT?");
+  }
+}
+
+void catSelection()
+{
+  srand(seed_time); // Set random with seed as the milliseconds until button 1 is pressed
+
+  printf("\n\nButton 1 - Next category");
+  printf("\nButton 2 - Select current category");
+  printf("\nCategories:");
+  printf("\n- Animal (ANML)");
+  printf("\n- Human (HMAN)");
+  printf("\n- Country (CTRY)");
+  printf("\n- Thing (THNG)");
+  printf("\n- Football club (CLUB)");
+  printf("\n- Company (COMP)");
+
+  while (!cat_selected)
+  {
+    writeString(categories[cat_id]);
+  }
+
+  strcpy(puzzle.category, categories[cat_id]);
+  strcpy(puzzle.word, words[cat_id][rand() % MAX_WORDS]);
+  puzzle.attempts = 0;
+  puzzle.time = 0;
+
+  hideConsonants(puzzle.word, visible_word);
+
+  button1_index = setIndex(1, visible_word);
+  button2_index = setIndex(2, visible_word);
+  button3_index = setIndex(3, visible_word);
+}
+
+void game()
+{
+  printf("\n\nButton 1 - Edit first underscore");
+  printf("\nButton 2 - Edit second underscore (if exists)");
+  printf("\nButton 3 - Edit third underscore (if exists)");
+
+  while (game_active) 
+  {
+    writeString(visible_word);
+    if (verify_time >= 60) {
+      if (strcmp(visible_word, puzzle.word) == 0) {
+        game_active = 0;
+        game_won = 1;
+      }
+      else 
+      {
+        verify_time = 0;
+        puzzle.attempts++;
+        incorrectSound();
+        hideConsonants(puzzle.word, visible_word);
+      }
+    }
+  }
+}
+
+void success()
+{
+  int minutes = puzzle.time / 60;
+  int seconds = puzzle.time - minutes*60;
+  printf("\n\nIn category %s the word %s was guessed in %d tries and a total time of %dmin and %dsec.",
+  puzzle.category, puzzle.word, puzzle.attempts, minutes, seconds);
+  printf("\nButton 1 - Play again");
+  correctSound();
+  
+  while (game_won) {
+    writeString("YES!");
+  }
+}
+
+int main() 
+{
+  setup();
 
   while (1)
   {
-    setup();  
-
-    printf("\nWelcome to the four-letter word quiz.");
-    printf("\nButton 1 - Begin");
-  
-    while (!button_pressed) 
-    {
-      writeString("CAT?");
-    }
-
-    srand(seed_time); // Set random with seed as the milliseconds until button 1 is pressed
-
-    printf("\nButton 1 - Next category");
-    printf("\nButton 2 - Select current category");
-    printf("\nCategories:");
-    printf("\n- Animal (ANML)");
-    printf("\n- Human (HMAN)");
-    printf("\n- Country (CTRY)");
-    printf("\n- Thing (THNG)");
-    printf("\n- Football club (CLUB)");
-    printf("\n- Company (COMP)");
-
-    while (!cat_selected)
-    {
-      writeString(categories[cat_id]);
-    }
-
-    strcpy(puzzle.category, categories[cat_id]);
-    strcpy(puzzle.word, words[cat_id][rand() % MAX_WORDS]);
-    puzzle.attempts = 0;
-    puzzle.time = 0;
-
-    hideConsonants(puzzle.word, visible_word);
-
-    button1_index = setIndex(1, visible_word);
-    button2_index = setIndex(2, visible_word);
-    button3_index = setIndex(3, visible_word);
-
-    printf("\nButton 1 - Edit first underscore");
-    printf("\nButton 2 - Edit second underscore (if exists)");
-    printf("\nButton 3 - Edit third underscore (if exists)");
-
-    while (game_active) 
-    {
-      writeString(visible_word);
-      if (verify_time >= 60) {
-        if (strcmp(visible_word, puzzle.word) == 0) {
-          game_active = 0;
-          game_won = 1;
-        }
-        else 
-        {
-          verify_time = 0;
-          puzzle.attempts++;
-          incorrectSound();
-          printf("\n%s is incorrent.", visible_word);
-          printf("\nCurrent time: %d seconds", puzzle.time);
-          printf("\nCurrent attempts: %d", puzzle.attempts);   
-          hideConsonants(puzzle.word, visible_word);
-        }
-      }
-    }
-    int minutes = puzzle.time / 60;
-    int seconds = puzzle.time - minutes*60;
-    printf("\nIn category %s the word %s was guessed in %d tries and a total time of %dmin and %dsec.",
-    puzzle.category, puzzle.word, puzzle.attempts, minutes, seconds);
-    printf("\nButton 1 - Play again");
-    correctSound();
-    
-    while (game_won) {
-      writeString("YES!");
-    }
+    start();
+    catSelection();
+    game();
+    success();
   }
 
-  return 0;
+  return 1;
 }
